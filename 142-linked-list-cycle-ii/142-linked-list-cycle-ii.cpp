c87f18ef3f327c8106a9431b6da547f88b1c5744
Arpita Nema <nemaarpita2012@gmail.com>
2022-07-04 00:19:50 +0530

    Time: 18 ms (27.13%), Space: 7.6 MB (34.77%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -10,7 +10,7 @@ class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
         
-       
+     /* M1   Space Complexity - O(n)
       ListNode *T=head;
         
        map <ListNode*, ListNode*> m;
@@ -27,6 +27,50 @@ public:
        }
         
        return T; 
+      */
+        
+        /* M2   Space Complexity - O(1)*/ 
+        
+        if((!head) || (!head->next)){
+            return NULL;
+        }
+        
+         ListNode *fast=head;
+         ListNode *slow=head;
+        
+        ListNode* prev =head;
+        bool flag = false;
+        while(fast&& fast->next ){
+            fast=fast->next->next;
+            slow=slow->next;
+            
+            if(fast==slow){  //this will happen at any node present within cycle
+                flag = true;
+                break;
+            } 
+        }
+        
+        //to find out the exact which node tail connects to...
+        
+        ListNode * detectNodePtr = head;
+        
+        if(flag){
+            
+            while(detectNodePtr!=fast){
+                fast=fast->next;
+                detectNodePtr=detectNodePtr->next;
+            }
+            
+            return detectNodePtr;
+               
+        }
+        
+        
+        return NULL;
+        
+       
+        
+        
         
         
     }
