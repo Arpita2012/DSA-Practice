18667e398104210d803f7f3ee3c199786a1803ce
Arpita Nema <nemaarpita2012@gmail.com>
2022-07-31 23:22:19 +0530

    Time: 31 ms (99.41%), Space: 19 MB (65.98%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& arr) {
-         sort(arr.begin(),arr.end());                // Sort on start time
+         sort(arr.begin(),arr.end());                
+        // Sort on start time
         int n = arr.size();
 
-        vector<vector<int>> ans;                            //  final answer vector
+        vector<vector<int>> ans;                            
+        //  final answer vector
         ans.push_back(arr[0]);
-        int lastIdx = 0;                                               // Last index of answer vector
+        int lastIdx = 0;                                               
+        // Last index of answer vector
 
         for(int i = 1; i < arr.size(); i++){
-            if(ans[lastIdx][1] >= arr[i][0]){                            // if the current interval overlap with
-                ans[lastIdx][1] = max(ans[lastIdx][1],arr[i][1]);        // lastIdx element of answer vector
-            }                                                            // then merge it to lastIdx element
+            if(ans[lastIdx][1] >= arr[i][0]){                            
+                // if the current interval overlap with
+                ans[lastIdx][1] = max(ans[lastIdx][1],arr[i][1]);        
+                // lastIdx element of answer vector
+            }                                                            
+            // then merge it to lastIdx element
             else{
-                ans.push_back(arr[i]);                                   // No overlap with lastIdx, then push 
+                ans.push_back(arr[i]);                                   
+                // No overlap with lastIdx, then push 
                 lastIdx++;
             }
         }
