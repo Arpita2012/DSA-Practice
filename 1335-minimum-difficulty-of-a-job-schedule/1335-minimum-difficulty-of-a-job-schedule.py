08f077a441e81e04255a9e2672ba15cb946c194e
Arpita Nema <nemaarpita2012@gmail.com>
2022-09-09 22:32:07 +0530

    Time: 5026 ms (12.70%), Space: 14.3 MB (63.09%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.py
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.py
@@ -1,20 +1,34 @@
-class Solution {
-public:
-    int minDifficulty(vector<int>& v, int d) {
-           int n=v.size();
-        v.insert(v.begin(),0);
-        vector<vector<int>> dp(n+1,vector<int>(d+1,1e7));
-        dp[0][0]=0;
-        for(int i=1;i<=n;i++){
-            for(int k=1;k<=min(i,d);k++){
-                int mx=INT_MIN;
-                for(int j=i;j>=k;j--){
-                    mx=max(mx,v[j]);
-                    dp[i][k]=min(dp[i][k],dp[j-1][k-1]+mx);
-                }
-            }
-        }
-       // cout<<dp[n][d]<<endl;
-        return (dp[n][d]==1e7)?-1:dp[n][d];
-    }
-};
+class Solution:
+    def minDifficulty(self, jobDifficulty: List[int], d: int) -> int:
+        
+        if d > len(jobDifficulty):
+            return -1
+        
+        elif d == len(jobDifficulty):
+            return sum(jobDifficulty)
+        
+        else:
+            return self.jS(jobDifficulty, d, {})
+    
+    def jS(self, jobDifficulty, d, mem):
+        
+        
+        if d == 1:
+            return max(jobDifficulty)
+        
+        if (d, len(jobDifficulty)) not in mem:
+        
+            min_el = float('Inf')
+            for i in range(0, len(jobDifficulty) - d + 1):
+
+                if i == 0:
+                    max_el = jobDifficulty[0]
+                else:
+                    max_el = max(jobDifficulty[0:i+1])
+
+                min_el = min(min_el, max_el + self.jS(jobDifficulty[i+1:], d - 1, mem))
+            
+            mem[(d, len(jobDifficulty))] = min_el
+        
+        return mem[(d, len(jobDifficulty))] 
+
